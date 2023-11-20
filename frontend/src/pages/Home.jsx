af34168644c1df7ef42257b7d7150548fa226dd5
Berkan Sözer <berkansozer@protonmail.com>
2023-11-20 08:22:48 +0300

    frontend can now render html string stored in backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
-import { Link, defer } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { Link } from "react-router-dom";
+import DOMPurify from "dompurify";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
@@ -24,17 +22,38 @@ const Home = () => {
       });
   }, []);
 
+  const renderPosts = (postContent) => {
+    const sanitizedPostContent = DOMPurify.sanitize(postContent);
+
+    return sanitizedPostContent;
+  };
+
   return (
-    <div className="p-4">
-      <div className="">
-        {posts.map((post, index) => {
-          return (
-            <div key={post._id}>
-              <p>{post.title}</p>;
-            </div>
-          );
-        })}
-      </div>
+    <div className="bg-gray-100 min-h-screen p-8 max-w-7xl">
+      <h1 className="text-3xl font-semibold mb-8">My Blog</h1>
+
+      {/* Loop through blog posts */}
+      {posts.map((post, index) => (
+        <div key={index} className="bg-white p-4 mb-8 rounded-lg shadow-md">
+          <h1 className="text-xl font-semibold mb-2">{post.title}</h1>
+          <p
+            dangerouslySetInnerHTML={{ __html: renderPosts(post.content) }}
+            className="text-gray-700 mb-4"
+          />
+
+          {/* Display tags */}
+          <div className="flex">
+            {post.tags.map((tag, tagIndex) => (
+              <span
+                key={tagIndex}
+                className="bg-blue-500 text-white px-2 py-1 mr-2 rounded"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
