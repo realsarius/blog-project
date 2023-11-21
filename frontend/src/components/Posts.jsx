370228119c9d42671625b3d511dbc0bf183cc7e6
Berkan Sözer <berkansozer@protonmail.com>
2023-11-21 12:08:23 +0300

    refactor: abstract blog post rendering logic into a reusable component
    Extracted the code responsible for rendering blog posts on the home page and individual post pages into a new component. This component intelligently handles both arrays and single posts as props, ensuring consistent rendering. This enhances code organization and promotes reusability.

---
+++ frontend/src/components/Posts.jsx
@@ -0,0 +1,135 @@
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineDelete } from "react-icons/md";
+import { Link } from "react-router-dom";
+import {
+  renderDates,
+  renderPostsLimitedCharacters,
+  renderTags,
+} from "../components/util/util";
+import PropTypes from "prop-types";
+
+const Posts = ({ posts }) => {
+  const checkPostOrPosts = () => {
+    if (posts.constructor == Object) {
+      return (
+        <div className="bg-gray-100 min-h-screen max-w-7xl">
+          <div
+            key={posts._id}
+            className="bg-white p-6 px-8 mb-8 rounded-lg shadow-md"
+          >
+            <div className="text-gray-600 text-xs uppercase tracking-widest">
+              {posts.createdAt === undefined
+                ? "undefined"
+                : renderDates(posts.createdAt)}
+            </div>
+            <h1 className="text-3xl text-gray-900 uppercase">{posts.title}</h1>
+
+            <p
+              dangerouslySetInnerHTML={{ __html: posts.content }}
+              className="text-gray-900 mb-4"
+            />
+
+            {/* Display tags */}
+            <div className="flex justify-between">
+              <div className="flex items-center italic text-gray-600">
+                Tags:
+                {posts.tags === undefined ? "Loading" : renderTags(posts.tags)}
+              </div>
+              {/* Admin controls */}
+              <div className="flex gap-4 justify-center">
+                <Link to={`/posts/details/${posts._id}`} className="flex gap-2">
+                  <BsInfoCircle
+                    className="text-2xl text-green-800"
+                    aria-label="info-button"
+                  />
+                  Info
+                </Link>
+                <Link to={`/posts/edit/${posts._id}`} className="flex gap-2">
+                  <AiOutlineEdit
+                    className="text-2xl text-yellow-600"
+                    aria-label="edit-button"
+                  />
+                  Edit
+                </Link>
+                <Link to={`/posts/delete/${posts._id}`} className="flex gap-2">
+                  <MdOutlineDelete
+                    className="text-2xl text-red-600"
+                    aria-label="delete-button"
+                  />
+                  Delete
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    if (posts.constructor == Array) {
+      return posts.map((post, index) => (
+        <div
+          key={index}
+          className="bg-white p-6 px-8 mb-8 rounded-lg shadow-md"
+        >
+          <div className="text-gray-600 text-xs uppercase tracking-widest">
+            {post.createdAt === undefined
+              ? "undefined"
+              : renderDates(post.createdAt)}
+          </div>
+          <Link to={`/posts/details/${post._id}`} className="flex gap-2">
+            <h1 className="text-3xl text-gray-900 uppercase">{post.title}</h1>
+          </Link>
+
+          <p
+            dangerouslySetInnerHTML={{
+              __html: renderPostsLimitedCharacters(post.content),
+            }}
+            className="text-gray-900 mb-4"
+          />
+
+          {/* Display tags */}
+          <div className="flex justify-between">
+            <div className="flex items-center italic text-gray-600">
+              Tags:
+              {post.tags === undefined ? "Loading" : renderTags(post.tags)}
+            </div>
+            {/* Admin controls */}
+            <div className="flex gap-4 justify-center">
+              <Link to={`/posts/details/${post._id}`} className="flex gap-2">
+                <BsInfoCircle
+                  className="text-2xl text-green-800"
+                  aria-label="info-button"
+                />
+                Info
+              </Link>
+              <Link to={`/posts/edit/${post._id}`} className="flex gap-2">
+                <AiOutlineEdit
+                  className="text-2xl text-yellow-600"
+                  aria-label="edit-button"
+                />
+                Edit
+              </Link>
+              <Link to={`/posts/delete/${post._id}`} className="flex gap-2">
+                <MdOutlineDelete
+                  className="text-2xl text-red-600"
+                  aria-label="delete-button"
+                />
+                Delete
+              </Link>
+            </div>
+          </div>
+        </div>
+      ));
+    }
+  };
+
+  return (
+    <>{checkPostOrPosts() === undefined ? "Loading" : checkPostOrPosts()}</>
+  );
+};
+
+Posts.propTypes = {
+  posts: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+};
+
+export default Posts;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
-import { Link } from "react-router-dom";
 import Navigation from "../components/Navigation";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineDelete } from "react-icons/md";
-import {
-  renderDates,
-  renderPostsLimitedCharacters,
-} from "../components/util/util";
+import Posts from "../components/Posts";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
@@ -31,76 +24,10 @@ const Home = () => {
 
   return (
     <div className="bg-gray-100 min-h-screen max-w-7xl">
-      {/* <div className="flex justify-between mb-8">
-        <h1 className="text-3xl font-semibold text-center">My Blog</h1>
-        <Link to={"/posts/create"}>
-          <MdOutlineAddBox className="text-4xl text-sky-800" />
-        </Link>
-      </div> */}
-
       <Navigation />
 
       {/* Loop through blog posts */}
-      {loading ? (
-        <Spinner />
-      ) : (
-        posts.map((post, index) => (
-          <div
-            key={index}
-            className="bg-white p-6 px-8 mb-8 rounded-lg shadow-md"
-          >
-            <div className="text-gray-600 text-xs uppercase tracking-widest">
-              {renderDates(post.createdAt)}
-            </div>
-            <Link to={`/posts/details/${post._id}`} className="flex gap-2">
-              <h1 className="text-3xl text-gray-900 uppercase">{post.title}</h1>
-            </Link>
-
-            <p
-              dangerouslySetInnerHTML={{
-                __html: renderPostsLimitedCharacters(post.content),
-              }}
-              className="text-gray-900 mb-4"
-            />
-
-            {/* Display tags */}
-            <div className="flex justify-between">
-              <div className="flex items-center italic text-gray-600">
-                Tags:
-                {post.tags.map((tag, tagIndex) => (
-                  <span key={tagIndex} className="px-2 py-1 mr-2 rounded">
-                    {tag}
-                  </span>
-                ))}
-              </div>
-              {/* Admin controls */}
-              <div className="flex gap-4 justify-center">
-                <Link to={`/posts/details/${post._id}`} className="flex gap-2">
-                  <BsInfoCircle
-                    className="text-2xl text-green-800"
-                    aria-label="info-button"
-                  />
-                  Info
-                </Link>
-                <Link to={`/posts/edit/${post._id}`} className="flex gap-2">
-                  <AiOutlineEdit
-                    className="text-2xl text-yellow-600"
-                    aria-label="edit-button"
-                  />
-                  Edit
-                </Link>
-                <Link to={`/posts/delete/${post._id}`} className="flex gap-2">
-                  <MdOutlineDelete
-                    className="text-2xl text-red-600"
-                    aria-label="delete-button"
-                  />
-                  Delete
-                </Link>
-              </div>
-            </div>
-          </div>
-        ))
-      )}
+      {loading ? <Spinner /> : <Posts posts={posts} />}
     </div>
   );
 };
