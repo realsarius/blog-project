87a427aee377799b35bfbaeba91795396d98bc1c
Berkan Sözer <berkansozer@protonmail.com>
2023-11-23 09:05:56 +0300

    initialized Sidebar component

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+const Sidebar = () => {
+  return (
+    <div className="w-3/12 h-min bg-white p-6 px-8 mb-8 rounded-lg shadow-md">
+      <img
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.istockphoto.com%2Fvectors%2Fdefault-placeholder-woman-vector-id844000348%3Fk%3D6%26m%3D844000348%26s%3D612x612%26w%3D0%26h%3D2QiYy7vaGEt7MlzlMnLPhUSbRNw2FzwoLc6lTXh-3vA%3D&f=1&nofb=1&ipt=30fe94e5b92e0aaa42fba77e980b8fc6bf5883fa4c3bd3a18084242ad78b3405&ipo=images"
+        alt="profile-picture"
+        className="w-48 mx-auto"
+      />
+      <p className="text-center">Jane Doe</p>
+      <p></p>
+    </div>
+  );
+};
+
+export default Sidebar;
