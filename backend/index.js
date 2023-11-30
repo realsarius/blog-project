090344a8b69690e39544578c2d46790313ea88b3
realsarius <berkansozer@protonmail.com>
2023-11-30 20:03:21 +0300

    moving credentials from config.js to .env

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,23 @@
-import express from "express";
-import { PORT, mongoDBURL } from "./config.js";
-import mongoose from "mongoose";
-import { Post } from "./models/postModel.js";
-import postsRoute from "./routes/postsRoute.js";
-import usersRouter from "./routes/usersRoute.js";
-import cors from "cors";
-import bodyParser from "body-parser";
+import express from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import postsRoute from './routes/postsRoute.js';
+import usersRouter from './routes/usersRoute.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 // Middleware for parsing request body
-app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(
   bodyParser.urlencoded({
-    limit: "50mb",
+    limit: '50mb',
     extended: true,
     parameterLimit: 50000,
   })
 );
-app.use(bodyParser.text({ limit: "200mb" }));
+app.use(bodyParser.text({ limit: '200mb' }));
 
 // Middleware for handling CORS Policy
 app.use(cors());
@@ -31,20 +30,20 @@ app.use(cors());
 //   })
 // );
 
-app.get("/", (request, response) => {
+app.get('/', (request, response) => {
   console.log(request);
-  return response.status(234).send("Goodbye, World!");
+  return response.status(234).send('Goodbye, World!');
 });
 
-app.use("/posts", postsRoute);
-app.use("/users", usersRouter);
+app.use('/posts', postsRoute);
+app.use('/users', usersRouter);
 
 mongoose
-  .connect(mongoDBURL)
+  .connect(process.env.mongoDBURL)
   .then(() => {
-    console.log("connected to database");
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
+    console.log('connected to database');
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening to port: ${process.env.PORT}`);
     });
   })
   .catch((error) => {
