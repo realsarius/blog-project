bdbeb1ac4776b1c1efe9a08b6d537273e579d3bd
realsarius <berkansozer@protonmail.com>
2023-12-01 16:25:18 +0300

    fixed styling issue in the about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,32 @@
-import Navigation from "../components/Navigation";
+import Navigation from '../components/Navigation';
 
 const About = () => {
   return (
-    <div className="">
+    <div className='bg-gray-100 min-h-screen max-w-7xl'>
       <Navigation />
-      <h1>About</h1>
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat commodi
+        numquam, aliquam libero quidem pariatur beatae nemo vitae minima dolor
+        corrupti illo non. Eaque rem ex et neque consectetur voluptate. Eligendi
+        rem praesentium adipisci dicta, corporis at vero qui repudiandae eos
+        saepe cumque laborum, odit animi natus, blanditiis ipsa ducimus quod ab
+        magnam aut necessitatibus nisi recusandae nihil. Corporis, veritatis!
+        Vel ex quaerat, quis dolorum laudantium eum sint voluptatem minima animi
+        molestiae enim neque maxime reiciendis velit fuga, quos voluptate a
+        harum. Nulla quis possimus deleniti molestias vero, perferendis
+        accusamus.
+      </p>
+
+      <p>
+        In veniam deserunt placeat qui nesciunt odio magnam eveniet explicabo
+        nihil, porro atque quas unde, nobis fugiat quis exercitationem sit ex
+        reiciendis consequatur iste alias quo impedit ea. Recusandae, accusamus.
+        Tenetur maiores, delectus nobis, officiis quam magni corrupti
+        consequatur laudantium dolor ipsa adipisci cupiditate, reprehenderit
+        nulla quia? Harum nemo neque cum error nulla labore vitae, blanditiis
+        placeat accusamus quasi similique?
+      </p>
     </div>
   );
 };
